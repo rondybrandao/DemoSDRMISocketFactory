8d328d1015b1f2b2f73c822bcfa26f6186d4e8fe
Arquilau Brandão <Arquilau Brando@ArquilauBrando>
2016-11-06 22:04:31 -0400

    novo commit

--- src/Login/Server/LoginServer.java
+++ src/Login/Server/LoginServer.java
@@ -5,29 +5,48 @@
  */
 package Login.Server;
 
-/**
- *
- * @author User
- */
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.ServerSocket;
 import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.RMIServerSocketFactory;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocketFactory;
 import login_implement.LoginImplement;
-public class LoginServer 
-{
- public static void main(String[]args)
- {
-     try 
-     {
+import loginform_rmi.LoginInterface;
+
+public class LoginServer implements RMIServerSocketFactory, Serializable {
+    
+    public LoginServer(){
+        try 
+        {
            Registry reg=LocateRegistry.createRegistry(1099);
-           LoginImplement lp=new LoginImplement();
-           reg.rebind("login", lp);
-           System.out.println("Servidor OK");
-           
-           
-     } 
-     catch (Exception ex) 
-     {
-     }
- }   
+           //LoginImplement lp=new LoginImplement();
+           //reg.rebind("login", lp);
+           System.out.println("Servidor registrando");     
+        } 
+        catch (Exception ex) 
+        {
+        }
+        try {
+            LoginInterface li = new LoginImplement(1099);
+            Naming.rebind("rmi://localhost:1099/login", li);
+            System.out.println("Login.Server.LoginServer.<init>()");
+        } catch (Exception e) {
+        }
+    }
+ 
+    @Override
+    public ServerSocket createServerSocket(int port) throws IOException {
+        
+        ServerSocketFactory factory = SSLServerSocketFactory.getDefault();
+        ServerSocket socket = factory.createServerSocket(port);
+        return socket;
+    }
+    public static void main(String[]args) {
+        new LoginServer();
+    }   
+
 }
